870681e6fa927857cf0ae4791d547516296f11ae
ArkadiKorotitshTARpv22 <opilane@HP-CZC2349HV7>
2024-09-25 13:42:46 +0300

    1ss

---
+++ TraahvIndividual/Controllers/HomeController.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TraahvIndividual.Models;
+using System.Net;
+using System.Net.Mail;
+using OfficeOpenXml;
+using OfficeOpenXml.Table;
+using System.Globalization;
+using System.Threading;
+using Stripe;
+
+namespace TraahvIndividual.Controllers
+{
+    public class HomeController : Controller
+    {
+        TrahvidContext db = new TrahvidContext();
+        public ActionResult Index(string lang)
+        {
+
+            string selectedLang = !string.IsNullOrEmpty(lang) ? lang : "est";
+            ViewBag.Language = selectedLang;
+
+            CultureInfo newCulture = new CultureInfo(selectedLang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            HttpContext.Items["lang"] = selectedLang;
+
+            return View();
+
+            //var penalties = db.Traahv.AsQueryable();  // Запрос к базе данных
+
+            //// Если введен номер машины для поиска, отфильтруем результаты
+            //if (!string.IsNullOrEmpty(searchCarNumber))
+            //{
+            //    penalties = penalties.Where(p => p.SoidukeNumber.Contains(searchCarNumber));
+            //}
+
+            //return View(penalties.ToList());  // Возвращаем результат в представление
+        }
+
+
+
+        [AuthorizeUser("finesender2000@gmail.com")]
+        public ActionResult Traahv(string lang)
+        {
+            var translations = new Dictionary<string, Dictionary<string, string>>
+    {
+        { "ru", new Dictionary<string, string>
+            {
+                { "VehicleNumber", "Номер автомобиля" },
+                { "OwnerName", "Имя владельца" },
+                { "OwnerEmail", "Электронная почта владельца" },
+                { "ViolationDate", "Дата нарушения" },
+                { "Speeding", "Превышение скорости" },
+                { "FineAmount", "Размер штрафа" },
+                { "AddNew", "Добавить новый" },
+                { "Edit", "Изменить" },
+                { "Delete", "Удалить" },
+                { "Send", "Удалить" },
+            }
+        },
+        { "eng", new Dictionary<string, string>
+            {
+                { "VehicleNumber", "Vehicle Number" },
+                { "OwnerName", "Owner's Name" },
+                { "OwnerEmail", "Owner's Email" },
+                { "ViolationDate", "Violation Date" },
+                { "Speeding", "Speeding" },
+                { "FineAmount", "Fine Amount" },
+                { "AddNew", "Add New" },
+                { "Edit", "Edit" },
+                { "Delete", "Delete" },
+                { "Send", "Удалить" },
+            }
+        },
+        { "est", new Dictionary<string, string>
+            {
+                { "VehicleNumber", "Sõiduki Number" },
+                { "OwnerName", "Omaniku Nimi" },
+                { "OwnerEmail", "Omaniku Epost" },
+                { "ViolationDate", "Rikkumisekuupaev" },
+                { "Speeding", "Kiiruse Ületamine" },
+                { "FineAmount", "Trahvi Suurus" },
+                { "AddNew", "Lisa uus" },
+                { "Edit", "Muuda" },
+                { "Delete", "Kustuta" },
+                { "Send", "Send" },
+            }
+        }
+    };
+
+            
+            string selectedLang = !string.IsNullOrEmpty(lang) ? lang : "est";
+            ViewBag.Language = selectedLang;
+
+            
+            ViewBag.Translations = translations[selectedLang];
+
+            IEnumerable<Traahv> traahvs = db.Traahv.ToList();
+            return View(traahvs);
+        }
+        [Authorize]
+        public ActionResult TraahvUsers(string lang = "est", string searchCarNumber = null)
+        {
+            var penalties = db.Traahv.AsQueryable();  
+
+            
+            if (!string.IsNullOrEmpty(searchCarNumber))
+            {
+                penalties = penalties.Where(p => p.SoidukeNumber.Contains(searchCarNumber));
+            }
+
+            string selectedLang = lang;
+            ViewBag.Language = selectedLang;
+
+            
+            CultureInfo newCulture = new CultureInfo(selectedLang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            HttpContext.Items["lang"] = selectedLang;
+
+            return View(penalties.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult CreateTraahv(string lang)
+        {
+            string selectedLang = !string.IsNullOrEmpty(lang) ? lang : "est";
+            ViewBag.Language = selectedLang;
+
+            
+            CultureInfo newCulture = new CultureInfo(selectedLang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            HttpContext.Items["lang"] = selectedLang;
+
+            return View();
+        }
+
+
+        public ActionResult CreateTraahv(Traahv trahv)
+        {
+            trahv.CalculateFine();
+            db.Traahv.Add(trahv);
+            db.SaveChanges();
+            trahv.SendMessage();
+            return RedirectToAction("Traahv");
+        }
+        
+        [HttpGet]
+        public ActionResult EditTraahv(int? id, string lang)
+        {
+            Traahv g = db.Traahv.Find(id);
+            if (g == null)
+            {
+                return HttpNotFound();
+
+            }
+            string selectedLang = !string.IsNullOrEmpty(lang) ? lang : "est";
+            ViewBag.Language = selectedLang;
+
+           
+            CultureInfo newCulture = new CultureInfo(selectedLang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            HttpContext.Items["lang"] = selectedLang;
+            return View(g);
+        }
+
+        [HttpGet]
+        public ActionResult SendTraahv(int? id, string lang)
+        {
+            Traahv g = db.Traahv.Find(id);
+            if (g == null)
+            {
+                return HttpNotFound();
+
+            }
+            string selectedLang = !string.IsNullOrEmpty(lang) ? lang : "est";
+            ViewBag.Language = selectedLang;
+
+
+            CultureInfo newCulture = new CultureInfo(selectedLang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            HttpContext.Items["lang"] = selectedLang;
+            g.SendMessage();
+            return RedirectToAction("Traahv");
+        }
+
+        [HttpPost, ActionName("EditTraahv")]
+        public ActionResult EditTraahvConfirmed(Traahv guest)
+        {
+            guest.CalculateFine();
+            db.Entry(guest).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Traahv");
+        }
+        public ActionResult DeleteTraahv(int id, string lang)
+        {
+            Traahv d = db.Traahv.Find(id);
+            if (d == null)
+            {
+                return HttpNotFound();
+            }
+
+            string selectedLang = !string.IsNullOrEmpty(lang) ? lang : "est";
+            ViewBag.Language = selectedLang;
+
+            // Set culture
+            CultureInfo newCulture = new CultureInfo(selectedLang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            HttpContext.Items["lang"] = selectedLang;
+
+            return View(d);
+        }
+
+        // POST: Delete the penalty
+        [HttpPost, ActionName("DeleteTraahvConfirmed")]
+        public ActionResult DeleteTraahvConfirmed(int id)
+        {
+            Traahv d = db.Traahv.Find(id);
+            if (d == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.Traahv.Remove(d);
+            db.SaveChanges();
+            return RedirectToAction("Index"); // Redirect to your list view
+        }
+        //public ActionResult Index(string searchCarNumber = null)
+        //{
+        //    var penalties = db.Traahv.AsQueryable();  // Запрос к базе данных
+
+        //    // Если введен номер машины для поиска, отфильтруем результаты
+        //    if (!string.IsNullOrEmpty(searchCarNumber))
+        //    {
+        //        penalties = penalties.Where(p => p.SoidukeNumber.Contains(searchCarNumber));
+        //    }
+
+        //    return View(penalties.ToList());  // Возвращаем результат в представление
+        //}
+        public ActionResult TrahvSearch(string searchCarNumber = null)
+        {
+            var penalties = db.Traahv.AsQueryable();  // Запрос к базе данных
+
+            // Если введен номер машины для поиска, отфильтруем результаты
+            if (!string.IsNullOrEmpty(searchCarNumber))
+            {
+                penalties = penalties.Where(p => p.SoidukeNumber.Contains(searchCarNumber));
+            }
+
+            return View(penalties.ToList());  // Возвращаем результат в представление
+        }
+        [HttpGet]
+        public ActionResult DetailTrahv(int id, string lang)
+        {
+            Traahv g = db.Traahv.Find(id);
+            if (g == null)
+            {
+                return HttpNotFound();
+            }
+
+            string selectedLang = !string.IsNullOrEmpty(lang) ? lang : "est";
+            ViewBag.Language = selectedLang;
+
+            // Устанавливаем язык в HttpContext
+            CultureInfo newCulture = new CultureInfo(selectedLang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            HttpContext.Items["lang"] = selectedLang;
+
+            return View(g);
+        }
+
+        public ActionResult ExportToExcel()
+        {
+            var traahvData = GetTraahvData();
+
+            // Устанавливаем лицензию
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Traahv Data");
+
+                // Добавляем заголовки
+                worksheet.Cells[1, 1].Value = "Soiduke Number";
+                worksheet.Cells[1, 2].Value = "Omaniku Nimi";
+                worksheet.Cells[1, 3].Value = "Omaniku Epost";
+                worksheet.Cells[1, 4].Value = "Rikkumise kuupaev";
+                worksheet.Cells[1, 5].Value = "Kiiruse Uletamine";
+                worksheet.Cells[1, 6].Value = "Trahvi Suurus";
+
+                // Заполняем данные
+                int row = 2;
+                foreach (var item in traahvData)
+                {
+                    worksheet.Cells[row, 1].Value = item.SoidukeNumber;
+                    worksheet.Cells[row, 2].Value = item.OmanikuNimi;
+                    worksheet.Cells[row, 3].Value = item.OmanikuEpost;
+                    worksheet.Cells[row, 4].Value = item.Rikkumisekuupaev.ToString("yyyy-MM-dd");
+                    worksheet.Cells[row, 5].Value = item.KiiruseUletamine;
+                    worksheet.Cells[row, 6].Value = item.TrahviSuurus;
+                    row++;
+                }
+
+                // Генерируем и возвращаем Excel-файл
+                var excelData = package.GetAsByteArray();
+                return File(excelData, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Trahvid.xlsx");
+            }
+        }
+
+        private IEnumerable<Traahv> GetTraahvData()
+        {
+            IEnumerable<Traahv> traahvs = db.Traahv.ToList();
+
+            return traahvs;
+
+        }
+
+        public ActionResult Charge()
+        {
+            StripeConfiguration.ApiKey = "sk_test_51PxGMLKVor0wWlI4O9xOczq92pZFhgIEcSfBD3E8LoYcSrKQ7T0m5Uf4PGggZoyG3Y4uwaNM3xM0NVKECbqqRjrz00ptuxVs70";
+
+            // В реальном проекте token должен быть сгенерирован Stripe
+            var token = "test_token"; // Тестовый токен
+
+            var options = new ChargeCreateOptions
+            {
+                Amount = 2000, // Сумма в центах (например, $20.00)
+                Currency = "usd",
+                Description = "Test Payment",
+                Source = token,
+            };
+
+            var service = new ChargeService();
+            Charge charge = service.Create(options);
+
+            if (charge.Status == "succeeded")
+            {
+                // Платёж прошёл успешно
+                return Content("Платёж успешно завершён!");
+            }
+            else
+            {
+                // Ошибка платежа
+                return Content("Ошибка при оплате.");
+            }
+        }
+        public ActionResult StripeForm(string lang)
+        {
+            string selectedLang = !string.IsNullOrEmpty(lang) ? lang : "est";
+            ViewBag.Language = selectedLang;
+
+            // Устанавливаем язык в HttpContext
+            CultureInfo newCulture = new CultureInfo(selectedLang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            HttpContext.Items["lang"] = selectedLang;
+            return View();
+        }
+    }
+
+
+
+}
---
+++ TraahvIndividual/Models/Traahv.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace TraahvIndividual.Models
+{
+    [Table("Traahv")]
+    public class Traahv
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "Vale SoidukeNumber")]
+        public string SoidukeNumber { get; set; }
+        [RegularExpression(@"^[a-zA-Zа-яА-ЯёЁ\s]{1,30}$", ErrorMessage = "Vale OmanikuNimi")]
+        public string OmanikuNimi { get; set; }
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Valesti sisestatud email")]
+        public string OmanikuEpost { get; set; }
+        public DateTime Rikkumisekuupaev { get; set; }
+        [Range(3, 300, ErrorMessage = "Valesti KiiruseUletamine")]
+        public int KiiruseUletamine { get; set; }
+
+        public int TrahviSuurus { get; set; }
+        public virtual ICollection<Login> Logins { get; set; }
+
+
+        public void CalculateFine()
+        {
+            if (KiiruseUletamine <= 10)
+            {
+                TrahviSuurus = 25;
+            }
+            else if (KiiruseUletamine <= 20)
+            {
+                TrahviSuurus = 45;
+            }
+            else if (KiiruseUletamine <= 45)
+            {
+                TrahviSuurus = 65;
+            }
+            else if (KiiruseUletamine <= 60)
+            {
+                TrahviSuurus = 120;
+            }
+            else if (KiiruseUletamine <= 80)
+            {
+                TrahviSuurus = 275;
+            }
+            else
+            {
+                TrahviSuurus = 400;
+            }
+        }
+        public void SendMessage()
+        {
+
+            try
+            {
+
+                var fromAddress = new MailAddress("finesender2000@gmail.com", "Police");
+                var toAddress = new MailAddress(OmanikuEpost, OmanikuNimi);
+                const string fromPassword = "tgwt xjrr yrea xida";
+                string message1 = "Trahv infromatsion " + SoidukeNumber;
+                string subject = message1;
+                string body = $"Tere, kell {Rikkumisekuupaev} rikute kiirusepiirangut ({KiiruseUletamine}), nii et te trahv {TrahviSuurus}. Maksmiseks on teil 2 kuud";
+
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                };
+
+                // Create the email message
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body
+                })
+                {
+                    // Send the email
+                    smtp.Send(message);
+                }
+
+                Console.WriteLine("Email sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to send email. Error: " + ex.Message);
+            }
+
+
+        }
+    }
+}
